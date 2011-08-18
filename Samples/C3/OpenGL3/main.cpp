017359f077032a3b1f6a9408f8a52f09c94f4a9d
beltegeuse <adrien.gruson@gmail.com>
2011-08-18 04:55:53 +0000

    Add OpenGL3Test.

--- Samples/C3/OpenGL3/main.cpp
+++ Samples/C3/OpenGL3/main.cpp
@@ -1,5 +1,188 @@
+#include <iostream>
+#include <C3/Window.h>
+
+#ifdef MSVC
+#include <windows.h>
+#endif
+#include <glload/gl_3_2_comp.h>
+
+
+GLuint CreateShader(GLenum eShaderType, const std::string &strShaderFile)
+{
+	GLuint shader = glCreateShader(eShaderType);
+	const char *strFileData = strShaderFile.c_str();
+	glShaderSource(shader, 1, &strFileData, NULL);
+
+	glCompileShader(shader);
+
+	GLint status;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+
+		const char *strShaderType = NULL;
+		switch(eShaderType)
+		{
+		case GL_VERTEX_SHADER: strShaderType = "vertex"; break;
+		case GL_GEOMETRY_SHADER: strShaderType = "geometry"; break;
+		case GL_FRAGMENT_SHADER: strShaderType = "fragment"; break;
+		}
+
+		std::cout << "Compile failure in " << strShaderType<< " shader:\n" << strInfoLog << "\n";
+		delete[] strInfoLog;
+	}
+
+	return shader;
+}
+
+GLuint CreateProgram(const std::vector<GLuint> &shaderList)
+{
+	GLuint program = glCreateProgram();
+
+	for(size_t iLoop = 0; iLoop < shaderList.size(); iLoop++)
+		glAttachShader(program, shaderList[iLoop]);
+
+	glLinkProgram(program);
+
+	GLint status;
+	glGetProgramiv (program, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+		std::cout << "Linker failure: " << strInfoLog << "\n";
+		delete[] strInfoLog;
+	}
+
+	return program;
+}
+
+GLuint theProgram;
+
+const std::string strVertexShader(
+	"#version 330\n"
+	"layout(location = 0) in vec4 position;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = position;\n"
+	"}\n"
+);
+
+const std::string strFragmentShader(
+	"#version 330\n"
+	"out vec4 outputColor;\n"
+	"void main()\n"
+	"{\n"
+	"   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+	"}\n"
+);
+
+void InitializeProgram()
+{
+	std::vector<GLuint> shaderList;
+
+	shaderList.push_back(CreateShader(GL_VERTEX_SHADER, strVertexShader));
+	shaderList.push_back(CreateShader(GL_FRAGMENT_SHADER, strFragmentShader));
+
+	theProgram = CreateProgram(shaderList);
+}
+
+const float vertexPositions[] = {
+	0.75f, 0.75f, 0.0f, 1.0f,
+	0.75f, -0.75f, 0.0f, 1.0f,
+	-0.75f, -0.75f, 0.0f, 1.0f,
+};
+
+GLuint positionBufferObject;
+GLuint vao;
+
+
+void InitializeVertexBuffer()
+{
+	glGenBuffers(1, &positionBufferObject);
+
+	glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+//Called after the window and OpenGL are initialized. Called exactly once, before the main loop.
+void init()
+{
+	InitializeProgram();
+	InitializeVertexBuffer();
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+}
 
 int main(int argc, char ** argv)
 {
+	// Object Creation
+	C3::Window win;
+	C3::WindowMode mode(800,600);
+	C3::OpenGLContextSettings openGLSettings(3,1);
+
+	// Create the window
+	std::cout << "Creation ..." << std::endl;
+	win.Create(mode,"OpenGL3",openGLSettings);
+
+	init();
+
+	// Initialise OpenGL
+	glViewport(0, 0, 800, 600);
+
+	// Draw loop
+	std::cout << "Affichage ..." << std::endl;
+	while(win.IsOpened())
+	{
+		// Events
+		C3::Event event;
+		while(win.PoolEvent(event))
+		{
+						//std::cout << "Event !" << std::endl;
+			if(event.Type == C3::Event::Closed)
+			{
+				std::cout << "Close ... " << std::endl;
+				win.Close();
+			}
+			else if(event.Type == C3::Event::KeyPressed)
+			{
+				if(event.Key.Code == C3::Key::Escape)
+				{
+					std::cout << "Close ... " << std::endl;
+					win.Close();
+				}
+			}
+		}
+
+		// Draw the scene
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(theProgram);
+
+		glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
+		glUseProgram(0);
+
+		// Swap buffers
+		win.Display();
+	}
+
+	std::cout << "... End" << std::endl;
 	return 0;
 }
